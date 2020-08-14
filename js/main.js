e72ef40694cfaf5356f9ea189f4e64a59790c367
stephen yi <stephenyi@stephens-MacBook-Pro.local>
2020-08-13 20:00:14 -0400

    update

--- js/main.js
+++ js/main.js
@@ -13,7 +13,10 @@ class Pokemon {
 let startButton = document.querySelector(".start")
 const scoreCounter = document.querySelector('h3')
 let pokeImg = document.querySelector('.pokemon')
-const pokeName = document.querySelector('pName')
+const pokeName = document.querySelector('.monName')
+const enterButton = document.querySelector('.enter')
+const guess = document.querySelector('.answer')
+let rightMon = document.querySelector('.image')
 
 const charmander = new Pokemon('Charmander', './images/silhouette_charmander.png', './images/charmander.png')
 const sandshrew = new Pokemon('Sandshrew', './images/silhouette_sandshrew.png', './images/sandshrew.png')
@@ -25,17 +28,34 @@ const allPokemon = [charmander, sandshrew, vulpix]
 /*----- event listeners -----*/
 
 startButton.addEventListener('click', startGame)
+enterButton.addEventListener('click', checkAnswer)
 
 /*----- functions -----*/
 
 function startGame() {
     let random = Math.ceil(Math.random() * 3) -1
-    pokeImg.src = allPokemon[random].silhouette
+    i = random
+    pokeImg.src = allPokemon[i].silhouette
+    pokeName.innerText = allPokemon[i].name
     playSound()
 }
 
 function playSound() {
-    let audio = document.createElement("audio")
-    audio.src="whosthatpokemon.wav"
+    let audio = document.createElement('audio')
+    audio.src='whosthatpokemon.wav'
     audio.play()
+}
+
+function checkAnswer() {
+    if(guess.value.toLowerCase() === pokeName.innerText.toLowerCase()) {
+        counter += 1
+        scoreCounter.innerText = ('Score: ' + counter)
+    } else if (counter > 0) {
+        counter -= 1
+        scoreCounter.innerText = ('Score: ' + counter)
+    }
+    rightMon.classList.add('imageFade')
+    pokeImg.src = allPokemon[i].imgfull
+    pokeName.style.display = 'block'
+    guess.value = ''
 }
